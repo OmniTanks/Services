43f0ee09e7f4770676385c1c0248c2375d9613c2
Jeffery Myers <JeffM2501@gmail.com>
2019-12-15 21:39:25 -0800

    start work on service endpoint

---
+++ Central/Controllers/GenerateAnonUserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Central.Models.Requests;
+using Central.Models.Responces;
+
+namespace Central.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GenerateAnonUserController : ControllerBase
+    {
+        // POST: GenerateAnonUser
+        [HttpPost]
+        public GenerateAnonUserResponce Post([FromBody] GenerateAnonUserRequest request)
+        {
+            GenerateAnonUserResponce responce = new GenerateAnonUserResponce();
+
+            using (var userDB = new Databases.UserDB())
+            {
+                string id = userDB.FindIDByMacAddress(request.MacAddress);
+                if (id != string.Empty)
+                {
+                    var user = userDB.GetUserByID(id);
+                    responce.Result = "OK";
+                    responce.UserID = user.ID;
+                    responce.UserName = user.Name;
+                    responce.AccessKey = user.Hash;
+                    responce.AuthenticationToken = "GenTOKEN"; // userDB.NewToken(user);
+                }
+                else
+                {
+                    string name = string.Empty;
+                    while (name == string.Empty || userDB.NameExists(name))
+                    {
+                        name = userDB.GenerateName();
+                    }
+
+                    responce.UserName = name;
+                }
+            }
+            return responce;
+        }
+    }
+}
---
+++ Central/Databases/UserDB.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+using Central.Models;
+
+namespace Central.Databases
+{
+    public class UserDB : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Access> Accesses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=centralservices;user=centusr");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        
+        public string FindIDByMacAddress(string addres)
+        {
+            var query = from usr in Users where usr.Email == addres.ToUpperInvariant() select usr;
+            var user = query.FirstOrDefault<User>();
+
+            if (user != null)
+                return user.ID;
+
+            return string.Empty;
+        }
+
+        public User GetUserByID(string id)
+        {
+            var query = from usr in Users where usr.ID == id.ToUpperInvariant() select usr;
+            var user = query.FirstOrDefault<User>();
+
+            if (user != null)
+                return user;
+
+            return null;
+        }
+
+        public bool NameExists(string name)
+        {
+            var query = from usr in Users where usr.Name == name select usr;
+            var user = query.FirstOrDefault<User>();
+
+            return user != null;
+        }
+
+        private string[] FirstNames = new string[]{ "Magic",
+                                                    "Lemon",
+                                                    "Sweetie",
+                                                    "Frosty",
+                                                    "Jewel",
+                                                    "Brilliant",
+                                                    "Stormy",
+                                                    "Twinkle",
+                                                    "Dewdrop",
+                                                    "Sky",
+                                                    "Pepper",
+                                                    "Dusty",
+                                                    "Fire",
+                                                    "Sparkle",
+                                                    "Midnight",
+                                                    "Crystal",
+                                                    "Cool",
+                                                    "Minty",
+                                                    "Cloud",
+                                                    "Citrus",
+                                                    "Peach",
+                                                    "Bubble",
+                                                    "Berry",
+                                                    "Light",
+                                                    "Beauty",
+                                                    "Rose",
+                                                    "Dawn",
+                                                    "Cocoa",
+                                                    "Summer",
+                                                    "Star",
+                                                    "Speedy",
+                                                    "Whirly",
+                                                    "Lucky",
+                                                    "Gold"
+                                                    };
+
+        private string[] LastNamed = new string[] { "Red",
+                                                    "Mint",
+                                                    "Flip",
+                                                    "Skipper",
+                                                    "Dahlia",
+                                                    "Tangy",
+                                                    "Pink",
+                                                    "Berry",
+                                                    "Poppy",
+                                                    "Bright",
+                                                    "Glow",
+                                                    "Sugar",
+                                                    "Dasher",
+                                                    "Muffin",
+                                                    "Dash",
+                                                    "Comet",
+                                                    "Onyx",
+                                                    "Glory",
+                                                    "Treat",
+                                                    "Apple",
+                                                    "Lilac",
+                                                    "Dreams",
+                                                    "Emerald",
+                                                    "Jubilee",
+                                                    "Joy",
+                                                    "Heart",
+                                                    "Eyes",
+                                                    "Sunset",
+                                                    "Pie",
+                                                    "Blueberry",
+                                                    "Flitter",
+                                                    "Magenta"
+                                                };
+
+
+        private string RandomElement(string[] contents)
+        {
+            return contents[new Random().Next(contents.Length)];
+        }
+
+        public string GenerateName()
+        {
+            return RandomElement(FirstNames) + RandomElement(LastNamed) + new Random().Next(12, 99).ToString();
+        }
+    }
+}
